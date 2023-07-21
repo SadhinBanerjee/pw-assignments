a5261708e77b4b5bafabc69dda65b244f899f43e
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-07-21 05:38:28 +0530

    Day 2 assignment task completed

---
+++ ass1.java
@@ -0,0 +1,50 @@
+1.   What is computer ?
+
+ans: A computer is a elecrtonic device which is used to take the raw data from the
+     user, process it by using arithmetic and logical operation and then giving the 
+     desired output/result as per the need of the user and it also save this
+     information or the result in computer memory for future needs.
+
+2.   What is RAM ?
+
+ans: RAM is a primary memory present in a computer system. It is used for storing
+     the data and information. RAM is volatile memory which means it cannot save
+     the data permanently. RAM contain the data as the power supply in on and if 
+     the power is cut then it will loss all it's data from the memory. RAM is very
+     fast in nature for sending the data and receiving the information from the 
+     processor.
+
+3.   Where is data stored in a computer ?
+
+ans: In computer memory the computer is used to store all it's data.
+     It has two types:- (i) Primary memory  (ii) Secondary memory. 
+     (i) Primary memory:- RAM, cache memory etc.
+     (ii) Secondary memory:- HDD, SSD etc.
+
+4.   What is that input device used to type text and numbers on a document in the 
+     computer system ?
+
+ans: We used keyboard as an input device to type text and numbers on a document 
+     in the computer system.
+
+5.   What are the output devices ?
+
+ans: In computer system "MONITORS", "SPEAKERS", "PRINTERS", "PROJECTORS" etc.
+     are the most common output devices used in computer system that shows output.
+
+6.   which is the input device that allows a user to move the cursor or pointer
+     on the screen ?
+
+ans: "Computer Mouse" is an input device that allows a user to move the cursor or 
+     pointer on the screen.
+
+7.   Which languange is directly understood by the computer without a translation
+     program ?
+
+ans: "BINARY LANGUAGE" is directly understood by the computer without a translation 
+     program. 
+
+8.   What are input devices ?
+
+ans: In computer system "KEYBOARD" "MOUSE" "JOYSTICK" "MICs" etc. are the most common
+     input devices used by the computer system to take input from the user.
