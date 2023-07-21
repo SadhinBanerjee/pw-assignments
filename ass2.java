438e3c53a3836a74403bd00d7cf25b2537e2cd9e
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-07-21 05:41:54 +0530

    Day 3 assignment task completed

---
+++ ass2.java
@@ -0,0 +1,64 @@
+1.   What is GIT ?
+
+ans: GIT is a version control system (vcs).
+     It is developed by Torvolds in 2005.
+     It is open source system.
+     It is used to track the coding changes.
+     It is used for storing the files on a computer and we can also use online hosts
+     like "GIT HUB", "BIT BUCKET" etc... for storing the large files and also to
+     maintain the files saved in GIT SERVER.
+     It is also used for collaborating between the developers who are working on 
+     a same project.
+
+2.   What do you understand by version control system ?
+    
+ans: Version Control System (VCS) is an open source control system. 
+     By using this system we can track our source files and also we manage our files
+     time to time.
+     It helps a developer team who are working for developing an application.
+     GIT is an version control system.
+
+3.   What is GIT HUB ?
+
+ans: GIT HUB is an example of GIT SERVER.
+     It is very famous for storing the large files on the cloud.
+     If the files are deleted from user local system and if their files are saved in
+     GIT HUB then the deleted files are easily accessed from the cloud, And the other
+     person who are working on the same project they can also access their files.
+     GIT HUB also maintain the files history.
+     For saving the files on GIT HUB first we save our files on a local repository on 
+     our system.
+
+4.   Mention some popular GIT hosting services ?
+
+ans: Some popular GIT hosting services are: 
+     GIT HUB, BIT BUCKET, GIT LAB, BAZZAR etc...
+
+5.   Different types of Version Control System ?
+
+ans: There are two types of Version Control System (VCS) :
+     (i) Centralized Version System (CVS) : It contain only one main repository.
+     (ii) Distributed Version Control System (DVS/DVCS) : It contains local repository on 
+     every single systems and it also have one main repository.
+
+6.   What benefits come with using GIT ?
+
+ans: There are various types of benefits come with using GIT some are as follows:-
+     (i)   We can save our files and codes on GIT for future use.
+     (ii)  By using GIT we can track the coding changes.
+     (iii) GIT help us to collaborate with developers for working on same project.
+     (iv)  By using GIT we also change our code time to time as per needs.
+     (v)   We can also check our coding history by using GIT. 
+
+7.   What is GIT Repository ?
+   
+ans: GIT repository is a storage location present on a GIT.         
+     GIT repository is used for storing the source files. By using "commit" command
+     we can add our source files into repository and if we want to make some changes
+     on our source files then we have to access those files from the repository, to 
+     access those files we use "update" command, and after updating our codes/files 
+     we again add those files on repository.
+
+8.   How can we intialize a repository in GIT ?
+
+ans: By using "PUSH" we can easily initialize a repository in GIT.
