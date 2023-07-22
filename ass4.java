9583cb758e905ee4bc5b07ba00f7992660677bf2
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-07-22 11:39:38 +0530

    Day 6 assignment task completed

---
+++ ass4.java
@@ -0,0 +1,52 @@
+1.   What is programming languages ?
+
+ans: Programming languages are used to write programs in a system to solve a problem.
+     It is a set of grammetical rules to solve a task.
+     Some examples of programming languages are: PHYTON, JAVA, C++, C etc.
+
+2.   Why do we need programming languages ?
+    
+ans: We need programming languages in many ways:
+     i>. It makes the human life better and easier, because the computer do multiple task
+         at once if we define them clearly.
+    ii>. It is used to solve day to day problems.  
+   iii>. Programming languages is needed in education fields, Technical fields, Bussiness fields
+         etc. 
+
+3.   What are the features of JAVA ?
+
+ans: There are many features in java some are specify below:- 
+     i>. Simple:- java is a simple language to learn.
+    ii>. Portable:- java is a portable language it means it can runs anywhere.
+   iii>. Independent:- java is an independent language because the code written in one platform the 
+	 same code runs in different platforms without giving any issue.
+    iv>. Secure:- java is very secure programming language.
+     v>. Oops:- java support oops concept.  
+
+4.   What is an object ?
+
+ans: An object is a real world entity. 
+     It is a instance of a class which means it can use the attributes and the method of a 
+     class.
+
+5.   What is an class ?
+
+ans: A class is a group of objects, having similar property and behaviour.
+     For ex. A Dog is a class. where, the particular name of the dog like Tommy is an object. 
+
+6.   Explain about the main method in java ?
+
+ans: The main() in java is the starting point of the program from where JVM start it's 
+     execution without the main() JVM cannot execute the program.
+     Syntax of java:
+                       public static void main(string[] args);
+    Here,
+    - "public" is the access specifier. It helps JVM to identify the execution point of program.
+    - "static" is the keyword. Whenever, the main() is call by using static keyword we don't need
+      to create object.
+    - "void" is the return type statement. It specify that the program cannot not return any value.
+    - "main" is a default signature which is predefined in java. It specify to execute the program
+      line by line and stop execution after completion of this method.
+    - "string[] args" state that whenever the program take some data from the user, the data is 
+      execute in string format.It accept group of string called as string array. args[] is the 
+      array name and it is of string type. It also take input in numbers form. 
