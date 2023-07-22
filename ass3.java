864e5c3c7b422b31765023c985600a7a9045a338
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-07-22 11:38:18 +0530

    Day four assignment task completed

---
+++ ass3.java
@@ -0,0 +1,41 @@
+1.  How to check if git is available in your system ?
+
+ans: We use "git version" command to check if git is available in our system.
+
+2.  How to intialize a new git repository ?
+
+ans: We use "git init" command to intialize a new git repository.  
+
+3.  How to tell git about your name and email ?
+
+ans: i>. We use [git config --global username "your name"] command to tell git about your name. 
+    ii>. We use [git config --global user.email "your email"] command to tell git about your email. 
+
+4.  How to add a file in staging area ?
+ 
+ans: We use "git add <filename> command to add a file in staging area.
+ 
+5.  How to remove a file from the staging area ?
+
+ans: We use "git rm--chached <filename>" command to remove a file from the staging area.
+
+6.  How to make a commit ?
+
+ans: We use [git commit -m "what we want to commit"] command to make a commit.
+
+7.  How to send your changes to a remote repository ?
+
+ans: We use "git pull" command to send our changes to a remote repository.
+
+8.  What is the difference between clone and pull ?
+
+ans: clone:- 
+     i>. We use "git clone <URL of the repository>" command for cloning.
+    ii>. We use this command when we want the same code located in remote repository in our local system.
+   iii>. We generally clone the code from repository to add some extra code, to change the code or to remove 
+         some code as per the need and then push it to the remote repository.
+
+     pull:-
+    i>. We use "git pull" command for pulling.
+   ii>. We use this command to take the recent changes of code happens in remote repository.
+  iii>. We usually pull the code from repository to update the code.	 	
